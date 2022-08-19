6822ef521ee91bd73d7c74569627b5ac729c1371
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-19 15:12:38 +0500

    update Ex36

--- HomeWork_19_08_22/Ex36_sum_not_mod2/Program.cs
+++ HomeWork_19_08_22/Ex36_sum_not_mod2/Program.cs
@@ -1,32 +1,31 @@
-﻿// // Ex34: Задайте массив заполненный случайными положительными трёхзначными числами.  
-// Напишите программу, которая покажет количество чётных чисел в массиве:  [345, 897, 568, 234] -> 2
+﻿// Ex36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19,        [-4, -6, 89, 6] -> 0
 
-void FillArraySize3 (int[] arr)           //задание массива случ. трехзн.числами и вывод их на экран в квадр.скоб. через ","
+void FillArray (int[] arr)           //задание массива случ. числами и вывод их на экран в квадр.скоб. через ","
 {
+    Console.Write();
     Console.Write("[");
     int length = arr.Length - 1;
     for (int i =0; i < length; i++)
     {
-        arr[i] = new Random().Next(100, 1000);
+        arr[i] = new Random().Next(-99, 100);
         Console.Write($"{arr[i]},  ");
     }
-    arr[length] = new Random().Next(100, 1000);
-    Console.Write($"{arr[length]}]");
+    arr[length] = new Random().Next(-99, 100);
+    Console.WriteLine($"{arr[length]}]");
 }
-void Mod2(int[] arr)                       // Подсчет чётных чисел в массиве
+void SumNotMod2(int[] arr)                       // Подсчет суммы элементов, стоящих на нечётных позициях в массиве
 {
-    int count = 0;
-    for (int i = 0; i < arr.Length; i++)
+    Console.Write();
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i+=2)
     {
-        if (arr[i] % 2 == 0)
-        {
-            count++;
-        }
+        sum = sum + arr[i];
     }
-    Console.WriteLine($"  ->   {count}");
+    Console.WriteLine($"Сумма элементов на нечетных позициях равна {sum}");
 }
 Console.Write("Введите размер массива, size = ");
 int size = Convert.ToInt32(Console.ReadLine());
-int[] arr34 = new int[size];
-FillArraySize3(arr34);
-Mod2(arr34);
+int[] arr36 = new int[size];
+FillArray(arr36);
+SumNotMod2(arr36);
