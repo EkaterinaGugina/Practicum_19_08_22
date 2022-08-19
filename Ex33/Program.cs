195bb39f45b52044f1d86635518949e16c56a8f3
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-20 01:01:33 +0500

    Practicum continuit

---
+++ Ex33/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+int[] arr(int size, int a, int b)             //"создание" массива
+{
+   int[] array = new int[size];
+   for(int i = 0; i < size; i++) array[i] = new Random().Next(a, b + 1);
+   return array;
+}
+
+void SeachElem (int[] arr, int elem)          //поиск элемента
+{
+bool label = false;
+for (int i = 0; i < arr.Length; i++) if (arr[i] == elem) label = true;
+if (label) Console.WriteLine($"да, элемент {elem} в массиве есть");
+else Console.WriteLine($"элемента {elem} в массиве нет");
+}
+
+Console.Write("Введите размер массива, size = ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array33 = arr(size, -9, 9);
+Console.WriteLine($"[{string.Join(" ", array33)}]");
+Console.Write("Введите элемент (число) для поиска его в массиве, elem = ");
+int elem = Convert.ToInt32(Console.ReadLine());
+SeachElem(array33, elem);
---
+++ Ex37/Program.cs
@@ -0,0 +1,26 @@
+﻿//*Задача 37: Найдите произведение пар чисел в одномерном массиве. 
+//Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+//[1 2 3 4 5] -> 5 8 3
+//[6 7 3 6] -> 36 21*/
+
+int[] arr(int num, int x, int y)
+{
+    int[] array = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = new Random().Next(x, y + 1);
+    }
+    return array;
+}
+void Mult(int[] arr)
+{
+    for (int i = 0; i < arr.Length/2; i++)
+    {
+        Console.Write($" {arr[i]*arr[arr.Length-1-i]} ");
+    }
+if(arr.Length % 2 != 0) Console.Write (arr[arr.Length/2]);
+}
+
+int[] array33 = arr(5, 1, 15);
+Console.WriteLine(string.Join(" ", array33));
+Mult(array33);
