07b7a86a1b8ea928aafff8d85273f83f4c86652e
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-20 01:42:15 +0500

    Ex37 solved

--- Ex37/Program.cs
+++ Ex37/Program.cs
@@ -3,24 +3,27 @@
 //[1 2 3 4 5] -> 5 8 3
 //[6 7 3 6] -> 36 21*/
 
-int[] arr(int num, int x, int y)
+int[] arr(int size, int a, int b)   //заполнение массива
 {
-    int[] array = new int[num];
-    for (int i = 0; i < num; i++)
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
     {
-        array[i] = new Random().Next(x, y + 1);
+        array[i] = new Random().Next(a, b + 1);
     }
     return array;
 }
+
 void Mult(int[] arr)
 {
     for (int i = 0; i < arr.Length/2; i++)
     {
-        Console.Write($" {arr[i]*arr[arr.Length-1-i]} ");
+        Console.Write($" {arr[i]*arr[arr.Length-1-i]}");
     }
 if(arr.Length % 2 != 0) Console.Write (arr[arr.Length/2]);
 }
 
-int[] array33 = arr(5, 1, 15);
-Console.WriteLine(string.Join(" ", array33));
-Mult(array33);
+Console.Write("Введите размер массива, size = ");  //заполнение и печать массива
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array37 = arr(size, 1, 10);
+Console.WriteLine($"[{string.Join(" ", array37)}]");
+Mult(array37);
