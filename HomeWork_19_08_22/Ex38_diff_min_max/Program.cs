b1a40cc54f74e4e1d2e430223937a8c6b7f0ac98
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-19 18:33:50 +0500

    update solution Ex38

--- HomeWork_19_08_22/Ex38_diff_min_max/Program.cs
+++ HomeWork_19_08_22/Ex38_diff_min_max/Program.cs
@@ -1,14 +1,10 @@
 ﻿// Задайте массив вещественных чисел. Найдите разницу между макс. и мин. элементами массива.
-// [3 7 22 2 78] -> 76
 
 void PrintArray (double[] arr)              //вывод на экран в "[  ]" через ";"
 {
     Console.Write("[");
     int length = arr.Length - 1;
-    for (int i =0; i < length; i++)
-    {
-        Console.Write($"{arr[i]};  ");
-    }
+    for (int i =0; i < length; i++) Console.Write($"{arr[i]};  ");
     Console.WriteLine($"{arr[length]}]");
 }
 
@@ -16,29 +12,20 @@ double[] FillArray (double[] arr)           //задание массива c к
 {    
     Console.WriteLine("Введите элементы массива через Enter");
     int length = arr.Length;
-    for (int i = 0; i < length; i++)
-    {
-        arr[i] = Convert.ToDouble(Console.ReadLine());
-    }
+    for (int i = 0; i < length; i++) arr[i] = Convert.ToDouble(Console.ReadLine());
     PrintArray(arr);
     return arr;
 }
 
-void DiffMinMax(double[] arr)               // Подсчет разности макс. и миним. элементов массива
+void DiffMinMax(double[] arr)               // Нахождение позиций мин. и макс. эл-тов и подсчет разности между макс. и миним.
 {
     int N = arr.Length - 1;
     int pos_min = 0;
     int pos_max = 0;
     for (int i = 1; i <= N; i++)
     {
-        if (arr[i] < arr[pos_min])
-            {
-                pos_min = i;
-            }
-        else if (arr[i] > arr[pos_max])
-            {
-                pos_max = i;
-            }
+        if (arr[i] < arr[pos_min]) pos_min = i;
+        else if (arr[i] > arr[pos_max])  pos_max = i;
     }
 Console.WriteLine($"Разность между минимальным и максимальными значениями массива = {arr[pos_max] - arr[pos_min]}");        
 }  
