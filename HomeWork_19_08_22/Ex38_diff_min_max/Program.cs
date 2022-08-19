b12f5f93baa72e014aeb1d206ce2c8fe0297852c
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-19 18:10:45 +0500

    Ex38 update

--- HomeWork_19_08_22/Ex38_diff_min_max/Program.cs
+++ HomeWork_19_08_22/Ex38_diff_min_max/Program.cs
@@ -1,18 +1,7 @@
 ﻿// Задайте массив вещественных чисел. Найдите разницу между макс. и мин. элементами массива.
 // [3 7 22 2 78] -> 76
 
-double[] FillArray (double[] arr)           //задание массива c консоли  
-{    
-    Console.WriteLine("Введите элементы массива через Enter");
-    int length = arr.Length;
-    for (int i = 0; i < length; i++)
-    {
-        arr[i] = Convert.ToDouble(Console.ReadLine());
-    }
-    return arr;
-}
-
-void PrintArray (double[] arr)              //вывод на экран в квадр.скоб. через ","
+void PrintArray (double[] arr)              //вывод на экран в "[  ]" через ";"
 {
     Console.Write("[");
     int length = arr.Length - 1;
@@ -23,29 +12,39 @@ void PrintArray (double[] arr)              //вывод на экран в кв
     Console.WriteLine($"{arr[length]}]");
 }
 
-double DiffMinMax(double[] arr)               // Подсчет разности максимального и минимального элементов массива
-{
-    int N = arr.Length - 1;
-    for (int i = 0; i < N; i++)
+double[] FillArray (double[] arr)           //задание массива c консоли  
+{    
+    Console.WriteLine("Введите элементы массива через Enter");
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
     {
-        int position_min = i;
-        for (int j = i + 1; j < arr.Length; j++)
-        {
-            if (arr[j] < arr[position_min])
-                {
-                    position_min = j;
-                }
-        }
-        double min = arr[i];
-        arr[i] = arr[position_min];
-        arr[position_min] = min;
+        arr[i] = Convert.ToDouble(Console.ReadLine());
     }
     PrintArray(arr);
-    return (arr[N] - arr[0]);
+    return arr;
 }
+
+void DiffMinMax(double[] arr)               // Подсчет разности макс. и миним. элементов массива
+{
+    int N = arr.Length - 1;
+    int pos_min = 0;
+    int pos_max = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        if (arr[i] < arr[pos_min])
+            {
+                pos_min = i;
+            }
+        else if (arr[i] > arr[pos_max])
+            {
+                pos_max = i;
+            }
+    }
+Console.WriteLine($"Разность между минимальным и максимальными значениями массива = {arr[pos_max] - arr[pos_min]}");        
+}  
+
 Console.Write("Введите размер массива, size = ");
 int size = Convert.ToInt32(Console.ReadLine());
 double[] arr38 = new double[size];
-double[] array38 = FillArray(arr38);
-double diff_min_max = DiffMinMax(arr38);
-Console.WriteLine($"Разность между минимальным и максимальными значениями массива равна = {diff_min_max}");
+arr38 = FillArray(arr38);
+DiffMinMax(arr38); 
