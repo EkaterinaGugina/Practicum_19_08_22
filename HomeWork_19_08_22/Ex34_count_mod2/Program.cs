9850cc76bd279d61281d06ea0d9ae4676a4aaf6d
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-20 02:00:50 +0500

    Ex34 changed

--- HomeWork_19_08_22/Ex34_count_mod2/Program.cs
+++ HomeWork_19_08_22/Ex34_count_mod2/Program.cs
@@ -1,30 +1,26 @@
 ﻿// Ex34: Задайте массив заполненный случайными положительными трёхзначными числами.  
 // Напишите программу, которая покажет количество чётных чисел в массиве:  [345, 897, 568, 234] -> 2
 
-void FillArraySize3 (int[] arr)           //задание массива случ. трехзн.числами и вывод их на экран в квадр.скоб. через ","
+void FillArraySize3 (int[] arr)           //задание массива случ. трехзн.числами и вывод его на экран в квадр.скоб. через пробел
 {
-    Console.Write("[");
-    int length = arr.Length - 1;
-    for (int i =0; i < length; i++)
+    for (int i =0; i < arr.Length; i++)
     {
         arr[i] = new Random().Next(100, 1000);
-        Console.Write($"{arr[i]},  ");
     }
-    arr[length] = new Random().Next(100, 1000);
-    Console.Write($"{arr[length]}]");
+    Console.Write($"[{string.Join("  ", arr)}]");
 }
+
 void Mod2(int[] arr)                       // Подсчет чётных чисел в массиве
 {
     int count = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        if (arr[i] % 2 == 0)
-        {
-            count++;
-        }
+        if (arr[i] % 2 == 0) count++;
     }
     Console.WriteLine($"  ->   {count}");
 }
+
+Console.Clear();
 Console.Write("Введите размер массива, size = ");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] arr34 = new int[size];
