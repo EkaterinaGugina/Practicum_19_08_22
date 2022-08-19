5c7d9f76ebb50a0d07d6d2bc8426b4944f78e08a
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-20 02:21:56 +0500

    Ex36 chenged

--- HomeWork_19_08_22/Ex36_sum_not_mod2/Program.cs
+++ HomeWork_19_08_22/Ex36_sum_not_mod2/Program.cs
@@ -1,26 +1,18 @@
 // Ex36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19,        [-4, -6, 89, 6] -> 0
 
-void FillArray (int[] arr)           //задание массива случ. числами и вывод их на экран в квадр.скоб. через ","
+void FillArray (int[] arr)    //задание массива случ.числами и вывод его на экран в квадр.скоб. через пробел
 {
-    Console.WriteLine();
-    Console.Write("[");
-    int length = arr.Length - 1;
-    for (int i =0; i < length; i++)
-    {
-        arr[i] = new Random().Next(-99, 100);
-        Console.Write($"{arr[i]},  ");
-    }
-    arr[length] = new Random().Next(-99, 100);
-    Console.WriteLine($"{arr[length]}]");
+    for (int i = 0; i < arr.Length; i++) arr[i] = new Random().Next(-99, 100);
+    Console.Write($"[{string.Join("  ", arr)}]");
 }
-void SumNotMod2(int[] arr)                       // Подсчет суммы элементов, стоящих на нечётных позициях в массиве
+void SumNotMod2(int[] arr)    // Подсчет суммы элементов, стоящих на нечётных позициях в массиве
 {
     Console.WriteLine();
     int sum = 0;
     for (int i = 1; i < arr.Length; i+=2)
     {
-        sum = sum + arr[i];
+        sum += arr[i];
     }
     Console.WriteLine($"Сумма элементов на нечетных позициях равна {sum}");
 }
