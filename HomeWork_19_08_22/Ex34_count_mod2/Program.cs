42df663c87498a49db3341de1e4e2c20f26a2061
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-20 02:05:03 +0500

    Ex34 add

--- HomeWork_19_08_22/Ex34_count_mod2/Program.cs
+++ HomeWork_19_08_22/Ex34_count_mod2/Program.cs
@@ -3,23 +3,15 @@
 
 void FillArraySize3 (int[] arr)           //задание массива случ. трехзн.числами и вывод его на экран в квадр.скоб. через пробел
 {
-    for (int i =0; i < arr.Length; i++)
-    {
-        arr[i] = new Random().Next(100, 1000);
-    }
+    for (int i =0; i < arr.Length; i++) arr[i] = new Random().Next(100, 1000);
     Console.Write($"[{string.Join("  ", arr)}]");
 }
-
 void Mod2(int[] arr)                       // Подсчет чётных чисел в массиве
 {
     int count = 0;
-    for (int i = 0; i < arr.Length; i++)
-    {
-        if (arr[i] % 2 == 0) count++;
-    }
+    for (int i = 0; i < arr.Length; i++) if (arr[i] % 2 == 0) count++;
     Console.WriteLine($"  ->   {count}");
 }
-
 Console.Clear();
 Console.Write("Введите размер массива, size = ");
 int size = Convert.ToInt32(Console.ReadLine());
